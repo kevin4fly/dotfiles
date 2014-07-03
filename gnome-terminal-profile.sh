1983a9f778489db62011fcaff3fdd148bf88087e
Kevin Yun <kevin4fly@gmail.com>
2014-07-04 01:47:18 +0800

    optimized gnome-terminal-profile.sh

--- gnome-terminal-profile.sh
+++ gnome-terminal-profile.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
 
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/palette "#121212121212:#F9F926267272:#8282B4B41414:#FDFD97971F1F:#5656C2C2D6D6:#8C8C5454FEFE:#464654545757:#CCCCCCCCC6C6:#121212121212:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2"
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/background_color "#121212121212"
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/foreground_color "#F8F8F8F8F2F2"
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/bold_color "#8A8ABEBEB7B7"
-gconftool-2 -s -t bool /apps/gnome-terminal/profiles/Default/bold_color_same_as_fg "false"
-gconftool-2 -s -t bool /apps/gnome-terminal/profiles/Default/use_theme_colors "false"
-gconftool-2 -s -t bool /apps/gnome-terminal/profiles/Default/use_theme_background "false"
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/font "Monospace 10"
+# the absolute path of gnome terminal profile: ~/.gconf/apps/gnome-terminal/profiles/
+# you can remove the profiles and replace it with what you like
+
+PROFILE=Default
+CONF_TOOL=gconftool-2
+CONF_PATH="/apps/gnome-terminal/profiles/$PROFILE"
+
+$CONF_TOOL -s -t string "$CONF_PATH/palette" "#121212121212:#F9F926267272:#8282B4B41414:#FDFD97971F1F:#5656C2C2D6D6:#8C8C5454FEFE:#464654545757:#CCCCCCCCC6C6:#121212121212:#E5E522222222:#A6A6E3E32D2D:#FCFC95951E1E:#C4C48D8DFFFF:#FAFA25257373:#6767D9D9F0F0:#F2F2F2F2F2F2"
+$CONF_TOOL -s -t string "$CONF_PATH/background_color" "#121212121212"
+$CONF_TOOL -s -t string "$CONF_PATH/foreground_color" "#F8F8F8F8F2F2"
+$CONF_TOOL -s -t string "$CONF_PATH/bold_color" "#8A8ABEBEB7B7"
+$CONF_TOOL -s -t bool "$CONF_PATH/bold_color_same_as_fg" "false"
+$CONF_TOOL -s -t bool "$CONF_PATH/use_theme_colors" "false"
+$CONF_TOOL -s -t bool "$CONF_PATH/use_theme_background" "false"
+$CONF_TOOL -s -t string "$CONF_PATH/font" "Monospace 10"
